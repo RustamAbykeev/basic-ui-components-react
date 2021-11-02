e78fc635578b7fd82c98593cd75907cef19c071a
RustamAbykeev <51760919+RustamAbykeev@users.noreply.github.com>
2021-11-02 20:12:07 +0600

    Deleted unusable files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+import Breadcrumb from "./components/breadcrumb";
+
+function App() {
+  return (
+    <div>
+    <Breadcrumb/>
+    <Breadcrumb></Breadcrumb>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/breadcrumb.jsx
@@ -0,0 +1,8 @@
+
+export default function Breadcrumb() {
+    return (
+            <h1>
+                Title h1
+            </h1>
+    )
+}
